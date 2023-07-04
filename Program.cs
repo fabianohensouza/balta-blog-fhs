0dc9957286ddd348a71c2b9e9ff6a578fd27db25
Fabiano Souza <fabianohensouza@gmail.com>
2023-07-04 07:12:35 -0300

    Test value inserted

--- Program.cs
+++ Program.cs
@@ -19,4 +19,5 @@ public class Secrets
     public string JwtTokenSecret { get; set; } = string.Empty;
     public string ApiKey { get; set; } = string.Empty;
     public string PrivateKey { get; set; } = string.Empty;
+    public string Test { get; set; } = string.Empty;
 }
